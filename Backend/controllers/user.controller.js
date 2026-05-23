3e40ccb0385b8f021b2bc0aee6f1f561f114dc96
Pragati Gupta <pragatigupta168@gmail.com>
2026-05-23 14:06:00 +0530

    commit

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,5 +1,25 @@
 const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
+
 
 module.exports.registerUser = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
+    const {firstname, lastname, email, password} = req.body;
+
+    const hasedpassword = await bcrypt.hash(password);
+
+    const user = await userService.createUser({
+        firstname,
+        lastname,
+        email,
+        password: hasedpassword
+    });
+
+    const token = user.generateAuthToken();
+
+    res.status(201).json({ token, user });
 }
